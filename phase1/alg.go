139629e0c5271ad00489a52e802612cea05656fb
Gabriele <gabriele@nulab.com>
2023-08-05 15:32:53 +0200

    [p1-all] Before running the phase1 alg, remove obvious 2-node cycles in form A->B abd B->A

--- phase1/alg.go
+++ phase1/alg.go
@@ -17,7 +17,7 @@ func (alg Alg) IsValid() bool {
 }
 
 func (alg Alg) Process(g *graph.DGraph, params graph.Params) {
-
+	removeTwoNodeCycles(g)
 	switch alg {
 	case Greedy:
 		execGreedy(g)
@@ -25,5 +25,28 @@ func (alg Alg) Process(g *graph.DGraph, params graph.Params) {
 		execDepthFirst(g)
 	default:
 		panic("cyclebreaking: unknown alg value")
+	}
+}
+
+func removeTwoNodeCycles(g *graph.DGraph) {
+	type pair [2]*graph.Node
+
+	seen := map[pair]bool{}
+	hide := graph.EdgeSet{}
+
+	for _, e := range g.Edges {
+		a, b := e.From, e.To
+		if seen[pair{a, b}] || seen[pair{b, a}] {
+			hide[e] = true
+		} else {
+			seen[pair{a, b}] = true
+		}
+	}
+	for e := range hide {
+		e.From.Out.Remove(e)
+		e.To.In.Remove(e)
+		g.Edges.Remove(e)
+
+		// todo: add to a separate list
 	}
 }
