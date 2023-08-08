58ca428af8994ea0492820fa42c025c61ea9bbc5
Gabriele <gabriele@nulab.com>
2023-08-08 21:55:30 +0200

    [p3] Add no-op ordering enum value

--- phase3/alg.go
+++ phase3/alg.go
@@ -7,7 +7,12 @@ import (
 type Alg uint8
 
 const (
-	GraphvizDot Alg = iota
+	// NoOrdering does nothing. Nodes won't be reordered in their layers to minimize edge crossings.
+	NoOrdering Alg = iota
+
+	// GraphvizDot implements the mincross heuristic used in dot. It attempts to minimize bilayer edge crossings
+	// by sweeping up and down the layers and applying ordering nodes based on their weighted medians.
+	GraphvizDot
 	_endAlg
 )
 
@@ -17,6 +22,8 @@ func (alg Alg) IsValid() bool {
 
 func (alg Alg) Process(g *graph.DGraph, params graph.Params) {
 	switch alg {
+	case NoOrdering:
+		return
 	case GraphvizDot:
 		execGraphvizDot(g, params)
 	default:
