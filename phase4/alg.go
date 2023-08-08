8595ddcac1bc246110c835394c1d702474b7269a
Gabriele <gabriele@nulab.com>
2023-08-08 21:56:14 +0200

    [p4] Improve documentation for VerticalAlign

--- phase4/alg.go
+++ phase4/alg.go
@@ -10,8 +10,8 @@ const (
 	// NoPositioning does nothing. Nodes won't be assigned any coordinates.
 	NoPositioning Alg = iota
 
-	// VerticalAlign aligns nodes in each layer around the center of the diagram.
-	// It's a simple and fast to implement algorithm for quick prototyping.
+	// VerticalAlign aligns nodes in each layer vertically around the center of the diagram.
+	// Works best for tree-like graphs with no back-edges.
 	VerticalAlign
 
 	// BrandesKoepf aligns nodes based on blocks and classes in O(V+E).
@@ -19,12 +19,11 @@ const (
 	BrandesKoepf
 
 	// NetworkSimplex sets X coordinates by constructing an auxiliary graph and solving it with the network simplex method.
-	// Layers in the auxiliary graph are X coordinates in the main graph.
-	// Might be time-intensive for graphs above a few dozen nodes.
+	// Layers in the auxiliary graph are X coordinates in the main graph. Might be time-intensive for graphs above a few dozen nodes.
 	NetworkSimplex
 
 	// SinkColoring is a variant of BrandesKöpf that aligns nodes based on vertical blocks starting from the bottom.
-	// It results in a larger drawing but with more long vertical edge paths. Runs in O(2kn) with 1 <= k <= maxshifts
+	// It results in a larger drawing but with more long vertical edge paths. Runs in O(2kn) with 1 <= k <= maxshifts.
 	SinkColoring
 	_endAlg
 )
