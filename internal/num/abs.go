1ad040538971b4896b5d5e95424799e16d48dfb7
Gabriele <10689839+vibridi@users.noreply.github.com>
2024-06-20 17:06:19 +0200

    Upgrade to Go 1.22, remove exp/constraints package (#17)

--- internal/num/abs.go
+++ internal/num/abs.go
@@ -1,8 +1,18 @@
 package num
 
-import "golang.org/x/exp/constraints"
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
 
-func Abs[T constraints.Integer](x T) T {
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Integer interface {
+	Signed | Unsigned
+}
+
+func Abs[T Integer](x T) T {
 	if x < 0 {
 		return -x
 	}
