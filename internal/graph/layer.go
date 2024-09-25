aed957ffb48d52983c599e85a52694b1ac05951b
Gabriele <10689839+vibridi@users.noreply.github.com>
2024-09-25 12:36:37 +0200

    [graph] Add Head() method to Layer

--- internal/graph/layer.go
+++ internal/graph/layer.go
@@ -16,7 +16,12 @@ func (layer *Layer) Len() int {
 	return len(layer.Nodes)
 }
 
-// Tail returns the last node in this layer, i.e. the node with greatest index
+// Head returns the first node in this layer, i.e. the node with index 0
+func (layer *Layer) Head() *Node {
+	return layer.Nodes[0]
+}
+
+// Tail returns the last node in this layer, i.e. the node with the greatest index
 func (layer *Layer) Tail() *Node {
 	return layer.Nodes[len(layer.Nodes)-1]
 }
